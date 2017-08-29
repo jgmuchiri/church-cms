7ba9602323f970fff0dbb1ccb89f55395843ded1
jgmuchiri <jgmuchiri@gmail.com>
2017-08-29 23:37:34 +0000

    translated password.blade.php

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -5,13 +5,13 @@
 @section('content')
 
     <h2 style="font-size: 22px;line-height: 28px;margin: 0 0 12px 0;">
-        Here is your password reset link
+        @lang("Here is your password reset link")
     </h2>
-    <p>You or someone has requested to reset password at {{env('APP_NAME')}}</p>
-    <a class="button" href="{{url()->to('password/reset/'.$token)}}">Click here to
-        to reset your password</a>
+    <p>@lang("You or someone has requested to reset password at") {{env('APP_NAME')}}</p>
+    <a class="button" href="{{url()->to('password/reset/'.$token)}}">@lang("Click here 
+        to reset your password")</a>
     <br>
 
-    If you did not make this request, please visit {{url()->to('/')}} and update your password
+    @lang("If you did not make this request, please visit") {{url()->to('/')}} @lang("and update your password")
 
 @endsection
