f7549767c32c9ee26defffea85e7e245b93a5d15
jgmuchiri <jgmuchiri@gmail.com>
2017-08-29 23:21:46 +0000

    translated setting.blade.php

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.template')
 @section('title')
-    System Settings
+    @lang("System Settings")
 @endsection
 @section('content')
     <div class="row-fluid">
@@ -9,7 +9,7 @@
         <div class="span10">
             <div class="widget-box no-top">
                 <div class="widget-title bg_lg"><span class="icon"><i class="icon-calendar"></i></span>
-                    <h5>Settings</h5>
+                    <h5>@lang("Settings")</h5>
                     <div class="buttons">
 
                     </div>
@@ -21,23 +21,23 @@
                             <i class="icon-warning"></i>
                         </div>
                         <p class="category small">
-                            All site configurations are managed in <code>.evn</code> file located in the root
-                            of your application.<br/>
+                            @lang("All site configurations are managed in") <code>.evn</code> @lang("file located in the root
+                            of your application.")<br/>
                             <span class="text-danger">
-                    Change these settings only if you know what you are doing!
+                    @lang("Change these settings only if you know what you are doing!")
                 </span>
                         </p>
                     </div>
                     <div class="alert alert-danger">
                         {!! Form::open(['url'=>'settings/backup']) !!}
-                        <button class="btn btn-warning"><i class="icon-database"></i> Backup First!</button>
+                        <button class="btn btn-warning"><i class="icon-database"></i> @lang("Backup First!")</button>
                         {!! Form::close() !!}
                     </div>
                     <div class="panel panel-default">
                         <div class="panel-body">
                             {!! Form::open() !!}
                             {!! Form::textarea('envContent',$envContent,['rows'=>20,'class'=>'span12']) !!}
-                            <button class="btn btn-default"><i class="icon-save"></i> Update</button>
+                            <button class="btn btn-default"><i class="icon-save"></i> @lang("Update")</button>
                             {!! Form::close() !!}
                         </div>
                     </div>
@@ -51,7 +51,7 @@
                     {{Form::file('logo')}}
                     <hr/>
 
-                    <button class="btn btn-success">Update</button>
+                    <button class="btn btn-success">@lang("Update")</button>
 
                     {!! Form::close() !!}
                 </div>
