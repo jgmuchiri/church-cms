c6425df607318a6231356d1369404e8836f646b7
jgmuchiri <jgmuchiri@gmail.com>
2017-08-29 23:13:21 +0000

    translated settings-menu.blade.php

--- resources/views/admin/settings-menu.blade.php
+++ resources/views/admin/settings-menu.blade.php
@@ -5,35 +5,35 @@
         </li>
         <li class="@if(Request()->segment(1)=="gift-options") active @endif">
             <a href="/giving/gift-options">
-                <i class="icon-chevron-right"></i> Gift Options </a>
+                <i class="icon-chevron-right"></i> @lang("Gift Options") </a>
         </li>
         <li class="@if(Request()->segment(1)=="menu") active @endif">
             <a href="/menu">
-                <i class="icon-chevron-right"></i> Main Menu </a>
+                <i class="icon-chevron-right"></i> @lang("Main Menu") </a>
         </li>
 
         <li class="@if(Request()->segment(1)=="themes") active @endif">
             <a href="/theme">
-                <i class="icon-chevron-right"></i> Theme </a>
+                <i class="icon-chevron-right"></i> @lang("Theme") </a>
         </li>
 
         <li class="@if(Request()->segment(1)=="roles") active @endif">
             <a href="/roles">
-                <i class="icon-chevron-right"></i> Roles </a>
+                <i class="icon-chevron-right"></i> @lang("Roles") </a>
         </li>
 
         {{--<li class="@if(Request()->segment(2)=="seo") active @endif">--}}
         {{--<a href="/settings/seo">--}}
-        {{--<i class="icon-chevron-right"></i> SEO </a>--}}
+        {{--<i class="icon-chevron-right"></i> @lang("SEO") </a>--}}
         {{--</li>--}}
 
         <li class="@if(Request()->segment(2)=="debug") active @endif">
             <a href="/debug-log">
-                <i class="icon-chevron-right"></i> Debug Log </a>
+                <i class="icon-chevron-right"></i> @lang("Debug Log") </a>
         </li>
         <li class="@if(Request()->segment(2)=="kiosk") active @endif">
             <a href="/kiosk">
-                <i class="icon-chevron-right"></i> Kiosk Mode </a>
+                <i class="icon-chevron-right"></i> @lang("Kiosk Mode") </a>
         </li>
     </ul>
 </div>
