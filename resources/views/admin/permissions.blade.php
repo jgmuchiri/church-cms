21626fcc886c55179ce9f10693e3b57a6b4fefc7
jgmuchiri <jgmuchiri@gmail.com>
2017-08-29 21:44:21 +0000

    translated permissions-ajax.blade.php

--- resources/views/admin/permissions.blade.php
+++ resources/views/admin/permissions.blade.php
@@ -1,9 +1,9 @@
-<a href="#" id="invert_selection"><i class="icon-check"></i> Inverse selection</a>
+<a href="#" id="invert_selection"><i class="icon-check"></i> @lang("Inverse selection")</a>
 {!! Form::open(['url'=>'update-role-permissions','class'=>'permissions']) !!}
 @foreach($rolePerms as $rp)
     <input type="checkbox" @if($rp['selected'] == true) checked @endif name="permissions[]"
            value="{{$rp['level']}}"> {{ucwords($rp['level'])}} <br/>
 @endforeach
 <br/>
-<button class="btn btn-default btn-sm">Update</button>
+<button class="btn btn-default btn-sm">@lang("Update")</button>
 {!! Form::close() !!}
