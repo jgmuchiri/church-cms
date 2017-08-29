34e8e01ccddd449e3a4f0473f8f2dd60ec50dcc7
jgmuchiri <jgmuchiri@gmail.com>
2017-08-29 23:42:23 +0000

    translated register.blade.php

--- resources/views/auth/emails/register.blade.php
+++ resources/views/auth/emails/register.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
     <h2 style="font-size: 22px;line-height: 28px;margin: 0 0 12px 0;">
-Please confirm your account
+@lang("Please confirm your account")
     </h2>
-    Welcome to {{env('APP_NAME')}}!
-    <p>Your account has been registered but we need you to take one final step to insure
-        someone else is not
-        trying to sign up using your email.</p>
+    @lang("Welcome to") {{env('APP_NAME')}}!
+    <p>@lang("Your account has been registered but we need you to take one final step to insure")
+        @lang("someone else is not")
+        @lang("trying to sign up using your email.")</p>
     <a href="{{ url('register/verify/' . $confirmation_code) }}" class="button">Verify Account</a>
     <br>
-    <p>Or copy paste this link to your browser {{ url('register/verify/' . $confirmation_code) }}</p>
+    <p>@lang("Or copy paste this link to your browser") {{ url('register/verify/' . $confirmation_code) }}</p>
 
 
-    If you did not sign up, please disregard this email or contact us at
+    @lang("If you did not sign up, please disregard this email or contact us at")
   {{env('EMAIL_FROM_ADDRESS')}}
 
 @endsection
