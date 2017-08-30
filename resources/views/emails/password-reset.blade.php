76aee75b0ce668ffa999811bb0931f78d6e77041
jgmuchiri <jgmuchiri@gmail.com>
2017-08-30 17:18:23 +0000

    translated password-reset.blade.php

--- resources/views/emails/password-reset.blade.php
+++ resources/views/emails/password-reset.blade.php
@@ -1,14 +1,14 @@
 @extends('emails.template')
 @section('header')
-    <h2>Your password reset link</h2>
+    <h2>@lang("Your password reset link")</h2>
 @endsection
 @section('content')
     <p></p>
-    You or someone requested to reset account password. If it was not you, it might be that
-    there was attempt to access the account. Please login and change your password.
+    @lang("You or someone requested to reset account password. If it was not you, it might be that")
+    @lang("there was attempt to access the account. Please login and change your password.")
 
-    <a href="{{ url('password/reset/' . $token) }}">Reset Your Account Password</a>.<br/>
+    <a href="{{ url('password/reset/' . $token) }}">@lang("Reset Your Account Password")</a>.<br/>
 
-    <p>Or copy paste this link to your browser {{ url('password/reset/' . $token) }}</p>
+    <p>@lang("Or copy paste this link to your browser") {{ url('password/reset/' . $token) }}</p>
 
 @endsection
