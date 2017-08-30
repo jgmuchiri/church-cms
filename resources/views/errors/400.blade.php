4e12a7c011dc1f6dd2ff4d542e181b8eb1d8196f
jgmuchiri <jgmuchiri@gmail.com>
2017-08-30 18:34:00 +0000

    translated 400.blade.php

--- resources/views/errors/400.blade.php
+++ resources/views/errors/400.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.template')
 
 @section('title')
-    Access denied
+    @lang("Access denied")
 @endsection
 @section('title-icon')exclamation
 @endsection
 
 @section('content')
-    <h3>You do not have permission to access that resource</h3>
-    <h3>Sorry :(</h3>
+    <h3>@lang("You do not have permission to access that resource")</h3>
+    <h3>@lang("Sorry") :(</h3>
 @endsection
