3a977a31fa348233b0d9da6d9926ae82bc08e4ec
jgmuchiri <jgmuchiri@gmail.com>
2017-08-30 18:30:58 +0000

    translated 202.blade.php

--- resources/views/errors/202.blade.php
+++ resources/views/errors/202.blade.php
@@ -1,9 +1,9 @@
 @extends('errors.template')
 
 @section('title')
-    Access Denied
+    @lang("Access Denied")
 @endsection
 
 @section('content')
-    <h3>Sorry, it looks like you ventured too far from safe-land. Retrace your steps back home</h3>
+    <h3>@lang("Sorry, it looks like you ventured too far from safe-land. Retrace your steps back home")</h3>
 @endsection
