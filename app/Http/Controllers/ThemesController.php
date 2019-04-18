ca6210df29dec6f82f865e68ac11eff81b0a1c7c
jgmuchiri <4142133+jgmuchiri@users.noreply.github.com>
2019-04-17 23:19:06 -0400

    minor namespace fixes

--- app/Http/Controllers/ThemesController.php
+++ app/Http/Controllers/ThemesController.php
@@ -1,12 +1,10 @@
 <?php
 namespace App\Http\Controllers;
 
-use App\Models\Settings;
 use App\Models\Themes;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use ZipArchive;
 
 class ThemesController extends Controller
 {
