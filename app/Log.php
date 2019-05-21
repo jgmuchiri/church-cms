69cb1549c9e95da8eaa22eb6777e35c5fbfe837e
jgmuchiri <4142133+jgmuchiri@users.noreply.github.com>
2019-05-21 07:34:55 -0400

    Update

--- app/Log.php
+++ app/Log.php
@@ -13,12 +13,16 @@ class Log extends Model
      * @param null $data
      * @internal param null $category
      */
-    public static function add($event,$action='update',$data=null){
-        if($data !==null){
+    public static function add($event, $action = 'update', $data = null)
+    {
+        if ($data !== null) {
             $data = serialize($data);
         }
-        $fp = fopen(storage_path().'/logs/logs.csv', 'w');
-        fputcsv($fp, array(date('Y-m-d H:i:s'),Auth::user()->id.': '.Auth::user()->name, $event,$data));
+
+        $fp = fopen(storage_path() . '/logs/logs.csv', 'w');
+
+        fputcsv($fp, array(date('Y-m-d H:i:s'), Auth::user()->id . ': ' . Auth::user()->name, $event, $data));
+
         fclose($fp);
     }
 
